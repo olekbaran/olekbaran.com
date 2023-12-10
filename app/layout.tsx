e33971fa4e74fc0191db95971fcdc5279db95992
Olek Baran <olekbaran24@gmail.com>
2023-12-10 21:40:20 +0100

    chore: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { type ReactNode } from "react";
+import "@/styles/globals.css";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  );
+}
