1b78200692932b589eb8365785949f530e7b6372
Olek Baran <olekbaran24@gmail.com>
2023-12-19 22:43:22 +0100

    refactor: link component (#7)

--- components/link.tsx
+++ components/link.tsx
@@ -1,13 +1,15 @@
 import { type AnchorHTMLAttributes } from "react"
 import NextLink, { LinkProps as NextLinkProps } from "next/link"
 
+import { cn } from "@/lib/utils"
+
 interface LinkProps
   extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof NextLinkProps>,
     NextLinkProps {}
 
-export function Link({ children, ...props }: LinkProps) {
+export function Link({ children, className, ...props }: LinkProps) {
   return (
-    <NextLink className="block h-8 overflow-hidden" {...props}>
+    <NextLink className={cn("block h-8 overflow-hidden", className)} {...props}>
       <div className="transition-transform duration-500 ease-in-out hover:-translate-y-1/2">
         <div>{children}</div>
         <div aria-hidden>{children}</div>
