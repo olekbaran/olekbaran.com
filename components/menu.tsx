2d9144623e0d57896dea1404980c3ee54fa9c464
Olek Baran <olekbaran24@gmail.com>
2023-12-23 14:13:58 +0100

    feat: menu items

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,4 +1,10 @@
+import { ArrowUpRightIcon } from "lucide-react"
+
+import { routes } from "@/config/routes"
 import { cn } from "@/lib/utils"
+
+import { Link } from "./link"
+import { Typography } from "./typography"
 
 interface MenuProps {
   pathname: string
@@ -9,11 +15,30 @@ export function Menu({ pathname, isOpen }: MenuProps) {
   return (
     <nav
       className={cn(
-        "container fixed inset-y-0 -translate-y-full border-b border-gray/10 bg-black transition-transform duration-500 ease-in-out md:hidden",
-        isOpen && "top-[calc(4rem+1px)] translate-y-0"
+        "container fixed inset-y-0 -translate-y-full overflow-y-auto border-b border-gray/10 bg-black py-16 transition-transform duration-500 ease-in-out md:hidden",
+        isOpen && "top-16 translate-y-0"
       )}
     >
-      Menu
+      <ul className="flex flex-col gap-10">
+        {Object.values(routes).map((route) => (
+          <li key={route.path} className="border-b border-gray px-5 pb-10">
+            <Link href={route.path} className="h-10">
+              <div className="flex items-center gap-5">
+                <Typography
+                  variant="h6"
+                  className={cn(
+                    "truncate",
+                    route.path === pathname ? "text-white" : "text-gray"
+                  )}
+                >
+                  {route.title}
+                </Typography>
+                <ArrowUpRightIcon className="h-8 w-8 shrink-0" />
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
