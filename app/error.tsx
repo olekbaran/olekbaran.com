d51c2f32b1fe8149646f40bb8599f8752e635cdf
Olek Baran <olekbaran24@gmail.com>
2023-12-26 15:46:30 +0100

    feat: error pages (#13)
    * feat: 404 page
    
    * feat: error page
    
    * feat: center error message text

---
+++ app/error.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Error as ErrorComponent } from "@/components/error"
+
+interface ErrorProps {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+export default function Error({ error }: ErrorProps) {
+  return (
+    <section className="container flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center">
+      <ErrorComponent
+        name={error.name}
+        message={error.message}
+        className="mb-16"
+      />
+    </section>
+  )
+}
---
+++ components/error.tsx
@@ -0,0 +1,24 @@
+import { type HTMLAttributes } from "react"
+
+import { cn } from "@/lib/utils"
+
+import { Typography } from "./typography"
+
+interface ErrorProps extends HTMLAttributes<HTMLElement> {
+  name: string
+  message: string
+}
+
+export function Error({ name, message, className, ...props }: ErrorProps) {
+  return (
+    <div
+      className={cn("flex flex-col items-center gap-5", className)}
+      {...props}
+    >
+      <Typography variant="h4">{name}</Typography>
+      <Typography variant="subtitle2" className="text-center text-gray">
+        {message}
+      </Typography>
+    </div>
+  )
+}
