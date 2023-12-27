ea84153d4d0a05a3162917721b665aa96c0b585f
Olek Baran <olekbaran24@gmail.com>
2023-12-27 12:31:46 +0100

    chore: better next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,13 @@ export const metadata: Metadata = metadataBase
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={syne.variable} suppressHydrationWarning>
-      <body suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <body className={syne.variable} suppressHydrationWarning>
         <div>
           <Header />
-          <main className="min-h-[calc(100vh-4rem)]">{children}</main>
+          <main className="flex min-h-[calc(100vh-4rem)] flex-col justify-center">
+            {children}
+          </main>
           <Footer />
         </div>
         <Analytics />
