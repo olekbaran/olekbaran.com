651311ca64f6549f76f44f4e80d0be4841f32177
Olek Baran <olekbaran24@gmail.com>
2023-12-29 11:44:10 +0100

    feat: project thumbnail

--- app/projects/project/page.tsx
+++ app/projects/project/page.tsx
@@ -1,3 +1,4 @@
+import NextLink from "next/link"
 import { ArrowUpRightIcon, GitPullRequestArrowIcon } from "lucide-react"
 
 import { routes } from "@/config/routes"
@@ -5,38 +6,45 @@ import { formatDate } from "@/lib/utils"
 import { Badge } from "@/components/badge"
 import { GoBack } from "@/components/go-back"
 import { Link } from "@/components/link"
+import { ProjectThumbnail } from "@/components/project-thumbnail"
 import { Typography } from "@/components/typography"
 
 const mockedProject = {
-  title: "Genemod",
+  title: "CallerSmart",
   isOpenSourceContribution: true,
   info: [
     {
+      type: "industry",
       label: "Industry",
       value: "Telecom",
     },
     {
+      type: "date",
       label: "Date",
       value: "2023-06-01",
     },
   ],
   links: [
     {
+      type: "repository",
       label: "Code",
       url: "",
     },
     {
+      type: "demo",
       label: "Demo",
       url: "",
     },
   ],
   technologies: ["React", "Next.js", "Tailwind CSS", "shadcn/ui"],
   images: [
-    "https://s3-alpha-sig.figma.com/img/0f63/7ea1/448b1ebb7851ef275a876c1962e99096?Expires=1704672000&Signature=d0zNl-V~YFYKLVtq2fQVS4bWx2gaN54aDD80KEYF5x4FxKwK0lYMRHb7xIcv8cT54efUt1lgoMY1yphSmFHuPgo9D8cSw2UY8DfUAnCd7lKJ1y92ZCB8nPbHfb5P5E4kl4hCAFnw5rlMr9385db7B8gPdrEofj1Z4-fZpNnvHl~6eNob0G5dpW~NylwP5X68MYC44Sp~nCe1UN5hjV8mxak9jkM0U9hNg-xSPcVhne2pIm4kR7UvTDdHGle0sUzwjCy5M3YyQmf65Eliz~c36pmb9INzHFFuR-NJmFCsNkby0Q7~edD4tbtskaLfuCsY3qUZJaFpn45jEd6MYWkkjg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    "https://s3-alpha-sig.figma.com/img/4ece/87d8/43acb64cac6383c6ee8299c5319b2cf3?Expires=1704672000&Signature=dGgw2vmKiAvKwxPem~hEkptH0Zwn06aoZ-OG169ex~pxY4qbRI1QIEQzTCygQeeEyj788P47Q3L5C3xiP7MxzgkITyWHQtlwW6YTHlu1s1wez8r6bCZNx5z63yT91O3ThPd5w00DsqNcwACCEmWMLjsjegCpLlXnvRHmhbIZZ5wFpgYMp7iNvlVHvdi8NOS2vatYje5h3I4LwI1YaAkYtq7gl~d4Lq8HHradBYfXRY4wnYufAHdJrAMTbz9WTpQh374oOOfBq6NeaoN4c0NIl-xLjgBHr~TcNlKtAB6Mk-vCCLzR9IW8vxVV6Y1KKQwVedxVjGYRc50bGWN9LAFGxQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   ],
 }
 
 export default function ProjectPage() {
+  const demoLink = mockedProject.links.find((link) => link.type === "demo")
+
   return (
     <section className="container flex flex-col gap-16 py-16 md:pb-32">
       <div className="flex flex-col gap-10">
@@ -64,7 +72,7 @@ export default function ProjectPage() {
             const isValueDate = Boolean(Date.parse(info.value))
 
             return (
-              <li key={info.label} className="flex flex-col gap-2">
+              <li key={info.type} className="flex flex-col gap-2">
                 <Typography variant="subtitle1" className="text-gray">
                   {info.label}:
                 </Typography>
@@ -77,7 +85,7 @@ export default function ProjectPage() {
         </ul>
         <ul className="flex flex-wrap items-center gap-10">
           {Object.values(mockedProject.links).map((link) => (
-            <li key={link.label}>
+            <li key={link.type}>
               <Link href={link.url} className="h-10">
                 <div className="flex items-center gap-2">
                   <Typography variant="h6" className="truncate">
@@ -90,6 +98,20 @@ export default function ProjectPage() {
           ))}
         </ul>
       </div>
+      {demoLink ? (
+        <NextLink href={demoLink.url} target="_blank" rel="noreferrer">
+          <ProjectThumbnail
+            title={mockedProject.title}
+            image={mockedProject.images[0]}
+            hasDemo
+          />
+        </NextLink>
+      ) : (
+        <ProjectThumbnail
+          title={mockedProject.title}
+          image={mockedProject.images[0]}
+        />
+      )}
     </section>
   )
 }
