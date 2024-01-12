41553cb1ed12a27f1b33e7a8597c3c8032a19fc8
Olek Baran <olekbaran24@gmail.com>
2024-01-12 13:41:32 +0100

    feat: types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,4 +3,11 @@ export type WorkExperience = {
   position: string
   startDate: string
   endDate?: string
+  companyWebsiteUrl?: string
+}
+
+export type GroupedWorkExperience = {
+  company: string
+  companyWebsiteUrl?: string
+  positions: Omit<WorkExperience, "company" | "companyWebsiteUrl">[]
 }
