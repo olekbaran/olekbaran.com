87a68c32dabf3c7bb55cbfbfc83e09a3d19d415e
Olek Baran <olekbaran24@gmail.com>
2024-01-13 18:41:19 +0100

    feat: brand identity

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,11 +2,17 @@ import { visionTool } from "@sanity/vision"
 import { defineConfig } from "sanity"
 import { structureTool } from "sanity/structure"
 
+import { Icon } from "./components/icon"
+import { siteConfig } from "./config/site"
 import { apiVersion, dataset, projectId } from "./sanity/env"
 import { schema } from "./sanity/schema"
 
 export default defineConfig({
   basePath: "/studio",
+  name: "studio",
+  title: `${siteConfig.name}`,
+  subtitle: "Content Studio",
+  icon: Icon,
   projectId,
   dataset,
   schema,
