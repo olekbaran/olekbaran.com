e5cf4407347c77941b6a768b5fcf9e7c69630e5a
Olek Baran <olekbaran24@gmail.com>
2024-02-16 18:52:24 +0100

    fix: page transition on dynamic routes

--- app/(user)/template.tsx
+++ app/(user)/template.tsx
@@ -1,15 +1,23 @@
-import { type ReactNode } from "react"
+"use client"
 
-import { MotionDiv } from "@/lib/framer"
+import { type ReactNode } from "react"
+import { usePathname } from "next/navigation"
+import { motion } from "framer-motion"
 
 interface TemplateProps {
   children: ReactNode
 }
 
 export default function Template({ children }: TemplateProps) {
+  const pathname = usePathname()
+
   return (
-    <MotionDiv initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }}>
+    <motion.div
+      key={pathname}
+      initial={{ opacity: 0, y: 40 }}
+      animate={{ opacity: 1, y: 0 }}
+    >
       {children}
-    </MotionDiv>
+    </motion.div>
   )
 }
