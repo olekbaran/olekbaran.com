fac80a28577beaa53f3adf5c6a0091a475ed5589
Olek Baran <olekbaran24@gmail.com>
2024-02-24 14:17:10 +0100

    feat: sanity studio validation

--- sanity/schemas/work-experience.ts
+++ sanity/schemas/work-experience.ts
@@ -10,21 +10,25 @@ export const workExperience = defineType({
       title: "Company",
       type: "reference",
       to: { type: "company" },
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "position",
       title: "Position",
       type: "string",
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "startDate",
       title: "Start date",
       type: "date",
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: "endDate",
       title: "End date",
       type: "date",
+      validation: (rule) => rule.min(rule.valueOfField("startDate")),
     }),
   ],
   preview: {
