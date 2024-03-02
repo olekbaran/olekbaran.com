5bacf3dab807ffab5129ec5aa631a2df02361b60
Olek Baran <olekbaran24@gmail.com>
2024-03-02 16:59:13 +0100

    fix: revalidate method

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,5 +1,6 @@
 import { draftMode } from "next/headers"
 import { redirect } from "next/navigation"
+import { type NextRequest } from "next/server"
 import { validatePreviewUrl } from "@sanity/preview-url-secret"
 
 import { client } from "@/sanity/lib/client"
@@ -7,7 +8,7 @@ import { token } from "@/sanity/lib/token"
 
 const clientWithToken = client.withConfig({ token })
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { isValid, redirectTo = "/" } = await validatePreviewUrl(
     clientWithToken,
     request.url
