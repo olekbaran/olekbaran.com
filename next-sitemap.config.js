243660a8bfa36b6ea53a5ccc6f626c16a5d94841
Olek Baran <olekbaran24@gmail.com>
2024-03-02 15:56:19 +0100

    feat: dynamic sitemap (#53)

---
+++ next-sitemap.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next-sitemap').IConfig} */
+const nextSitemapConfig = {
+  siteUrl: process.env.NEXT_PUBLIC_APP_URL,
+  generateIndexSitemap: false,
+  exclude: ["/server-sitemap.xml", "/projects/*"],
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/studio",
+      },
+    ],
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_APP_URL}/server-sitemap.xml`,
+    ],
+  },
+}
+
+export default nextSitemapConfig
