c20d566bac67939c259cd55fe04fdb83433f2899
Olek Baran <olekbaran24@gmail.com>
2024-03-02 15:11:24 +0100

    feat: better revalidate scope (#52)

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,11 +2,17 @@ import { revalidatePath } from "next/cache"
 import { type NextRequest } from "next/server"
 import { parseBody } from "next-sanity/webhook"
 
+import { routes } from "@/config/routes"
 import { revalidateWebhookSecret } from "@/sanity/lib/token"
+
+interface RevalidateWebhookBody {
+  _type: string
+  slug?: string
+}
 
 export async function POST(request: NextRequest) {
   try {
-    const { isValidSignature } = await parseBody(
+    const { body, isValidSignature } = await parseBody<RevalidateWebhookBody>(
       request,
       revalidateWebhookSecret
     )
@@ -15,7 +21,19 @@ export async function POST(request: NextRequest) {
       return new Response("Invalid secret", { status: 401 })
     }
 
-    revalidatePath("/", "layout")
+    if (!body?._type) {
+      return new Response("Bad request", { status: 400 })
+    }
+
+    Object.values(routes)
+      .filter((route) => !route.pathname.includes("/#"))
+      .forEach((route) => {
+        revalidatePath(route.pathname)
+      })
+
+    if (body._type === "project" && body.slug) {
+      revalidatePath(`${routes.projects.pathname}/${body.slug}`)
+    }
 
     return new Response("Successfull revalidation", { status: 200 })
   } catch {
