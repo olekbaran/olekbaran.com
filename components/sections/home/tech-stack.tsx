6d1d5da8377dd5ee9a8cc7c1005bea3fe360390c
Olek Baran <olekbaran24@gmail.com>
2024-03-21 14:15:36 +0100

    feat(tech stack): sticky heading (#68)

--- components/sections/home/tech-stack.tsx
+++ components/sections/home/tech-stack.tsx
@@ -13,18 +13,19 @@ export async function TechStack() {
   return (
     <section
       id="tech-stack"
-      className="container flex flex-col gap-16 py-16 md:py-32"
+      className="container grid gap-16 py-16 lg:grid-cols-2 lg:gap-10 lg:pb-32 lg:pt-[88px]"
     >
       <Heading
         title="Tech stack"
-        subtitle="Explore the cutting-edge tools powering my projects. My go-to tech stack that I use to create top-notch web applications."
+        subtitle="Explore the cutting-edge tools I use to make awesome web applications."
+        className="lg:sticky lg:top-16 lg:self-start lg:pt-10"
       />
       <LiveQueryWrapper<Technology[]>
         initial={initialTechnologies}
         isEnabled={isEnabled}
         query={isEnabled ? TECHNOLOGIES_QUERY : undefined}
       >
-        <ul className="flex flex-col gap-10">
+        <ul className="flex flex-col gap-10 overflow-hidden">
           {initialTechnologies.data.map((technology) => (
             <li key={technology._id}>
               <TechnologyCard name={technology.name} size="large" />
