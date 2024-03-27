ad789fb62270d92faf618a5afac584df35ee6c83
Olek Baran <olekbaran24@gmail.com>
2024-03-27 16:55:20 +0100

    fix: add local memoji image

--- components/common/memoji.tsx
+++ components/common/memoji.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 
+import memojiImage from "@/public/images/memoji.png"
+
 export function Memoji() {
   return (
     <div className="relative w-fit pt-20">
       <Image
-        src="/images/memoji.png"
+        src={memojiImage}
         alt=""
-        width={388}
-        height={549}
         quality={100}
         priority
         className="relative z-10"
