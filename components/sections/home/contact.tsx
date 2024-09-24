3f4cb5a47161d988b41c2eba5cfc7b364da727e4
Olek Baran <olekbaran24@gmail.com>
2024-09-24 18:09:08 +0200

    feat(nav): contact section observer (#90)
    * feat(nav): contact section observer
    
    * fix: lockfile

--- components/sections/home/contact.tsx
+++ components/sections/home/contact.tsx
@@ -1,10 +1,30 @@
+"use client"
+
+import { useInView } from "react-intersection-observer"
+
 import { siteConfig } from "@/config/site"
 import { ContactCard } from "@/components/cards/contact-card"
 import { Heading } from "@/components/typography/heading"
+import { useNavStore } from "@/stores/nav-store"
 
 export function Contact() {
+  const { setIsContactSectionActive, setIsContactSectionInactive } =
+    useNavStore((state) => state)
+
+  const { ref } = useInView({
+    threshold: 0.69,
+    onChange: (inView) => {
+      if (inView) {
+        setIsContactSectionActive()
+      } else {
+        setIsContactSectionInactive()
+      }
+    },
+  })
+
   return (
     <section
+      ref={ref}
       id="contact"
       className="container flex flex-col gap-16 py-16 md:py-32"
     >
