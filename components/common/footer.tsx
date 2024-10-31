7ca059228f0d4f7d790ce85129254f4a262c7403
Olek Baran <olekbaran24@gmail.com>
2024-10-31 23:55:05 +0100

    feat: better get in touch button

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,12 +1,8 @@
 "use client"
 
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react"
-
-import { routes } from "@/config/routes"
-
 import { SocialLinks } from "../buttons/social-links"
-import { Link } from "../nav/link"
 import { Typography } from "../typography/typography"
+import { GetInTouch } from "./get-in-touch"
 import { Logo } from "./logo"
 
 export function Footer() {
@@ -14,20 +10,9 @@ export function Footer() {
 
   return (
     <footer className="w-full border-t border-gray/10 bg-black py-10 md:pt-20">
-      <div className="container flex flex-col gap-10 md:gap-20">
-        <Link
-          href={routes.contact.pathname}
-          className="h-14 w-full rounded-2xl border border-gray md:h-24 md:rounded-3xl"
-        >
-          <div className="flex items-center justify-center gap-10 px-5 py-2 lg:justify-between lg:px-10">
-            <ArrowRightIcon className="hidden h-16 w-16 stroke-1 lg:block" />
-            <Typography variant="h3" className="truncate">
-              Get in touch
-            </Typography>
-            <ArrowLeftIcon className="hidden h-16 w-16 stroke-1 lg:block" />
-          </div>
-        </Link>
-        <div className="flex flex-col-reverse items-center justify-between gap-10 md:flex-row">
+      <div className="flex flex-col gap-10 md:gap-20">
+        <GetInTouch />
+        <div className="container flex flex-col-reverse items-center justify-between gap-10 md:flex-row">
           <div className="flex flex-col items-center gap-5 md:items-start">
             <Logo />
             <Typography variant="body2" className="text-center text-gray">
---
+++ components/common/get-in-touch.tsx
@@ -0,0 +1,57 @@
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react"
+
+import { routes } from "@/config/routes"
+import { cn } from "@/lib/utils"
+
+import { Link } from "../nav/link"
+import { Typography } from "../typography/typography"
+import { DuctTape } from "./duct-tape"
+
+interface DuctTapeContentProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+function DuctTapeContent({ className, ...props }: DuctTapeContentProps) {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center gap-5 px-5 py-2 md:gap-10 lg:justify-between lg:px-10",
+        className
+      )}
+      {...props}
+    >
+      <ArrowRightIcon className="h-6 w-6 stroke-1 md:h-14 md:w-14" />
+      <Typography
+        variant="h4"
+        className="truncate font-extrabold uppercase leading-none"
+      >
+        Get in touch
+      </Typography>
+      <ArrowLeftIcon className="h-6 w-6 stroke-1 md:h-14 md:w-14" />
+    </div>
+  )
+}
+
+function GetInTouch() {
+  return (
+    <div className="relative overflow-hidden py-[3vw]">
+      <Link
+        href={routes.contact.pathname}
+        className="absolute bottom-auto h-11 w-full rotate-3 scale-110 transform-gpu md:h-[5.5rem] md:scale-105"
+        aria-hidden
+      >
+        <DuctTape variant="secondary">
+          <DuctTapeContent className="ml-40" />
+        </DuctTape>
+      </Link>
+      <Link
+        href={routes.contact.pathname}
+        className="h-11 w-full -rotate-3 scale-110 transform-gpu md:h-[5.5rem] md:scale-105"
+      >
+        <DuctTape>
+          <DuctTapeContent />
+        </DuctTape>
+      </Link>
+    </div>
+  )
+}
+
+export { GetInTouch, DuctTapeContent }
