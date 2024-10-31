591332fff61e514183a964460435509b33efc690
Olek Baran <olekbaran24@gmail.com>
2024-10-31 23:57:15 +0100

    feat: better get in touch button (#109)

---
+++ components/common/duct-tape.tsx
@@ -0,0 +1,33 @@
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+import { ParallaxHorizontal } from "../utils/parallax-horizontal"
+
+const ductTapeVariants = cva("w-full", {
+  variants: {
+    variant: {
+      primary: "bg-white text-black",
+      secondary: "bg-blue text-white",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+  },
+})
+
+interface DuctTapeProps
+  extends VariantProps<typeof ductTapeVariants>,
+    React.HTMLAttributes<HTMLElement> {}
+
+function DuctTape({ variant, className, children, ...props }: DuctTapeProps) {
+  return (
+    <div className={cn(ductTapeVariants({ variant }), className)} {...props}>
+      <ParallaxHorizontal>
+        <div className="mx-10 flex items-center gap-20">{children}</div>
+      </ParallaxHorizontal>
+    </div>
+  )
+}
+
+export { DuctTape, ductTapeVariants }
