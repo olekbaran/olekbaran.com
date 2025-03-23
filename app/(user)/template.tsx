43fdc571178c40d73d1e9516d39da05e8e687e01
Olek Baran <olekbaran24@gmail.com>
2025-03-23 10:49:20 +0100

    feat: add loader (#141)

--- app/(user)/template.tsx
+++ app/(user)/template.tsx
@@ -1,7 +1,11 @@
 "use client"
 
 import { usePathname } from "next/navigation"
+import { ProgressProvider } from "@bprogress/next/app"
 import { motion } from "framer-motion"
+import resolveConfig from "tailwindcss/resolveConfig"
+
+import tailwindConfig from "@/tailwind.config"
 
 interface TemplateProps {
   children: React.ReactNode
@@ -10,13 +14,22 @@ interface TemplateProps {
 export default function Template({ children }: TemplateProps) {
   const pathname = usePathname()
 
+  const { theme } = resolveConfig(tailwindConfig)
+
   return (
-    <motion.div
-      key={pathname}
-      initial={{ opacity: 0, y: 40 }}
-      animate={{ opacity: 1, y: 0 }}
+    <ProgressProvider
+      height="1px"
+      color={theme.colors.white}
+      options={{ showSpinner: false, easing: "ease" }}
+      shallowRouting
     >
-      {children}
-    </motion.div>
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        {children}
+      </motion.div>
+    </ProgressProvider>
   )
 }
